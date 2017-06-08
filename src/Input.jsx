855b8267680931c710c483ed4d28f1b65ff8f08d
Salil Oak <salil.oak@allscripts.com>
2017-06-08 23:22:00 +0530

    Added Input as a component to send in dynamic values for each input

---
+++ src/Input.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+    bindInputChange(id, event) {
+        let newValue = {
+            id: id,
+            value: event.target.value
+        }
+        this.props.inputValue(newValue);
+    }
+    render() {
+        return (
+            <div>
+                <input id={this.props.inputId} type={this.props.inputType} placeholder={this.props.inputPlaceholder} ref={this.props.refsValue} onChange={this.bindInputChange.bind(this, this.props.inputId)} />
+            </div>
+        )
+    }
+}
+
+export default Input;
